d0bf36266f0f01c607266215e06e6c850555c84e
Rachel Mahan <rachelm2490@gmail.com>
2021-09-28 15:00:52 -0400

    Add fun fact dialog

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
 import MuiDataTable from "mui-datatables";
 import inventorsJson from "../data/inventors.json";
+import Button from '@material-ui/core/Button';
+import Dialog from "@material-ui/core/Dialog"
+import DialogTitle from "@material-ui/core/DialogTitle"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogActions from "@material-ui/core/DialogActions"
 
-type InventorData = { inventors: { first: string, last: string, year: number }[] }
+type UiInventor = { id: number, firstName: string, funFact: string, knownFor: string[], lastName: string, year: number };
+type OpenModalState = { isOpen: true, inventor?: UiInventor };
 
 export const DataTable = () => {
+  const [factModalState, setFactModalState] = React.useState<{ isOpen: false } | OpenModalState>({ isOpen: false });
+  const inventorsData = React.useMemo<UiInventor[]>(() => inventorsJson.inventors.map(invntr => ({
+    id: invntr.id,
+    firstName: invntr.first,
+    lastName: invntr.last,
+    year: invntr.year,
+    knownFor: invntr.known_for,
+    funFact: invntr.fun_fact
+  })), [])
   const columns = [
     {
       name: "firstName",
@@ -26,30 +43,45 @@ export const DataTable = () => {
         sort: true
       }
     },
+    {
+      name: "knownFor",
+      label: "Known For"
+    }
   ];
 
-  const inventorsData: InventorData = inventorsJson;
-  const data = inventorsData.inventors.map((invntr) => ({
-    firstName: invntr.first,
-    lastName: invntr.last,
-    year: invntr.year
-  }));
+  const handleRowClick = (rowData: string[]) => setFactModalState({ isOpen: true, inventor: inventorsData.find(invntr => invntr.firstName === rowData[0]) });
+  const handleModalClose = () => setFactModalState({ isOpen: false })
 
   return (
     <>
       <div id="table-container">
         <MuiDataTable
           title="Inventors"
-          data={data}
+          data={inventorsData}
           columns={columns}
           options={{
             download: false,
+            onRowClick: handleRowClick,
             print: false,
             selectableRows: "none",
             tableId: "inventors-table"
           }}
         />
       </div>
+      {factModalState.isOpen && (
+        <Dialog open={true} onClose={handleModalClose} aria-labelledby="fun-fact-dialog-title">
+          <DialogTitle id="fun-fact-dialog-title">Did you know??</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <p><b>{factModalState?.inventor?.firstName}&nbsp;{factModalState?.inventor?.lastName}</b></p>
+              <p>{factModalState?.inventor?.funFact}</p>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleModalClose}>Ok, got it</Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </>
   );
 }
